4d1f030e00f8b521df21910681aa60046f2733a2
Lgrella <60951320+Lgrella@users.noreply.github.com>
2023-10-22 22:07:21 -0400

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use my_project::Database;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let db = Database::new()?;
+    let data = db.get_all_data()?;
+
+    for item in data {
+        println!("{}", item);
+    }
+
+    Ok(())
+}
